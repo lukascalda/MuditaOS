d28e156f0e12a09dae2b55a7c47a4b46b3b309c5
Pawel Olejniczak <pawel.olejniczak@mudita.com>
2022-01-04 22:51:07 +0100

    [CP-855] Refactor test_threads
    Refactored threads part of messages endpoint tests.
    Rewritten with use of threads EP API from harness.

--- test/pytest/service-desktop/test_threads.py
+++ test/pytest/service-desktop/test_threads.py
@@ -1,35 +1,124 @@
-# Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+# Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 # For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 import pytest
-from harness.interface.defs import status
+from harness.request import TransactionError
+from harness.api.messages import GetThreadsWithOffsetAndLimit, MarkThreadAsUnread, GetMessageById, AddMessage, DeleteMessageById
+
+
+class ThreadsTester:
+    def __init__(self, harness):
+        self.harness = harness
+
+    def get_threads_with_offset_and_limit(self, offset, limit):
+        try:
+            result = GetThreadsWithOffsetAndLimit(offset, limit).run(self.harness)
+        except TransactionError:
+            return False
+        else:
+            return True, result
+
+    def mark_thread_as_unread(self, thread_id, set_thread_unread):
+        try:
+            result = MarkThreadAsUnread(thread_id, set_thread_unread).run(self.harness)
+        except TransactionError:
+            return False
+        else:
+            return True, result
+
+    def get_message_by_id(self, message_record_id):
+        try:
+            result = GetMessageById(message_record_id).run(self.harness)
+        except TransactionError:
+            return False
+        else:
+            return True, result.message
+
+    def add_message(self, message_number, message_body):
+        try:
+            message = AddMessage(message_number, message_body).run(self.harness).message
+        except TransactionError:
+            return False
+        else:
+            return True, message
+
+    def delete_message_by_id(self, message_record_id):
+        try:
+            DeleteMessageById(message_record_id).run(self.harness)
+        except TransactionError:
+            return False
+        else:
+            return True
 
 
 @pytest.mark.service_desktop_test
 @pytest.mark.usefixtures("phone_unlocked")
-def test_threads(harness):
-    # getting all threads
-    body = {"category": "thread"}
-    ret = harness.endpoint_request("messages", "get", body)
-    assert ret["status"] == status["OK"]
+def test_marking_thread_as_read(harness):
+    message_number_1 = "123456789"
+    message_1_body = "Test message nr 1"
 
-    # getting a number of threads
-    number_of_requested_threads = 3
-    if ret["body"]["totalCount"] < number_of_requested_threads:
-        number_of_requested_threads = ret["body"]["totalCount"]
-    body = {"category": "thread", "limit": number_of_requested_threads, "offset": 0}
-    ret = harness.endpoint_request("messages", "get", body)
-    assert ret["status"] == status["OK"]
+    threads_tester = ThreadsTester(harness)
 
-    threads = ret["body"]["entries"]  # getting entries
-    threads_count = len(threads)
-    assert threads_count == number_of_requested_threads
+    result, message_1_record = threads_tester.add_message(message_number_1, message_1_body)
+    assert result, "Failed to add message!"
+    result, received_message_1_record = threads_tester.get_message_by_id(message_1_record["messageID"])
+    assert result, "Failed to get message by id!"
 
-    # Check if all fields are present and have proper type
-    message_types = [1, 2, 4, 8, 16, 18, 255]
-    print(ret["body"])
-    for thread in ret["body"]["entries"]:
-        print(thread)
-        assert type(thread["contactID"]) == int
+    assert threads_tester.mark_thread_as_unread(received_message_1_record["threadID"], False)
+    assert threads_tester.mark_thread_as_unread(received_message_1_record["threadID"], True)
+
+    assert threads_tester.delete_message_by_id(message_1_record["messageID"]), "Failed to delete a message!"
+
+
+@pytest.mark.service_desktop_test
+@pytest.mark.usefixtures("phone_unlocked")
+def test_getting_threads(harness):
+    message_number_1 = "123456789"
+    message_number_2 = "223456789"
+    message_number_3 = "323456789"
+    message_number_4 = "423456789"
+    message_number_5 = "523456789"
+    message_number_6 = "623456789"
+    message_1_body = "Test message nr 1"
+    message_2_body = "Test message nr 2"
+    message_3_body = "Test message nr 3"
+    message_4_body = "Test message nr 4"
+    message_5_body = "Test message nr 5"
+    message_6_body = "Test message nr 6"
+
+    threads_tester = ThreadsTester(harness)
+
+    result, message_1_record = threads_tester.add_message(message_number_1, message_1_body)
+    assert result, "Failed to add message!"
+    result, message_2_record = threads_tester.add_message(message_number_2, message_2_body)
+    assert result, "Failed to add message!"
+    result, message_3_record = threads_tester.add_message(message_number_3, message_3_body)
+    assert result, "Failed to add message!"
+    result, message_4_record = threads_tester.add_message(message_number_4, message_4_body)
+    assert result, "Failed to add message!"
+    result, message_5_record = threads_tester.add_message(message_number_5, message_5_body)
+    assert result, "Failed to add message!"
+    result, message_6_record = threads_tester.add_message(message_number_6, message_6_body)
+    assert result, "Failed to add message!"
+
+    result, response = threads_tester.get_threads_with_offset_and_limit(
+        0, 0)
+    assert result, "Failed to get all threads!"
+    total_count_of_threads = response.totalCount
+    result, response = threads_tester.get_threads_with_offset_and_limit(
+        total_count_of_threads - 6, 6)
+    assert result, "Failed to get threads with offset and limit!"
+    THREADS_PAGE_SIZE = 4
+    assert len(response.threads) == THREADS_PAGE_SIZE
+    assert response.nextPage["limit"] == 2
+    assert response.nextPage["offset"] == total_count_of_threads - 2
+    received_threads = response.threads
+    result, response = threads_tester.get_threads_with_offset_and_limit(
+        response.nextPage["offset"], response.nextPage["limit"])
+    assert result, "Failed to get threads with offset and limit!"
+    received_threads += response.threads
+    assert len(received_threads) == 6
+
+    for thread in received_threads:
         assert type(thread["isUnread"]) == bool
         assert type(thread["lastUpdatedAt"]) == int
         assert type(thread["messageCount"]) == int
@@ -37,9 +126,5 @@ def test_threads(harness):
         assert type(thread["messageType"]) == int
         assert type(thread["number"]) == str
         assert type(thread["threadID"]) == int
+        message_types = [1, 2, 4, 8, 16, 18, 255]
         assert thread["messageType"] in message_types
-
-    # set thread as read
-    body = {"category": "thread", "threadID": 1, "isUnread": False}
-    ret = harness.endpoint_request("messages", "put", body)
-    assert ret["status"] == status["NoContent"]
